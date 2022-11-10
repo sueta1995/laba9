648ce828b1482234b5c8df9a63c1805812ea4e71
sueta1995 <kitaev.tagir@gmail.com>
2022-11-10 15:20:56 +0300

    added ajax

--- app/controllers/calculates_controller.rb
+++ app/controllers/calculates_controller.rb
@@ -5,13 +5,19 @@ class CalculatesController < ApplicationController
   def new; end
 
   def create
-    @numbers = params.require(:calculates).permit(:numbers)[:numbers].split(' ')
+    @numbers = params[:numbers].split(' ')
 
     if ::CalculatesController.valid?(@numbers)
       @result = ::CalculatesController.calculating(@numbers.collect(&:to_i))
       @max_result = @result.max_by(&:length)
 
-      render :results
+      respond_to do |format|
+        format.html
+        format.json do
+          render json:
+          { type: @result.class.to_s, input: @numbers.collect(&:to_i), all_sequences: @result, max_sequence: @max_result }
+        end
+      end
     else
       flash.now[:alert] = 'Введены не верные значения!'
 
